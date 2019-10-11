b2abfe5592e571556b4a29a8d0633d9146480951
Khalil Daya <khalildaya@Khalils-MacBook-Pro.local>
2019-10-11 18:21:39 -0400

    starts text filtering

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-function SearchBar() {
+function SearchBar(props) {
     return (
         <div>
             <div>
-                <input type="text" placeholder="Search..."></input>
+                <input type="text" placeholder="Search..." onChange={props.onFilterTextChange}></input>
             </div>
             <div>
                 <input id="showOnlyInStock" type="checkbox"></input>
@@ -37,7 +37,7 @@ function ProductTable(props) {
         const rows = [];
         data.forEach(products => {
             rows.push(<ProductCategoryRow key={products.category} category={products.category} />);
-            products.items.map(product => {
+            products.items.forEach(product => {
                 rows.push(<ProductRow key={product.name} product={product} />)
             });
         });
@@ -61,16 +61,45 @@ class FilterableProductTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            originalData: this.props.data,
             filteredData: this.props.data,
             showOnlyInStock: false,
             filterText: '',
         };
+        this.originalData = this.props.data;
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    }
+    handleFilterTextChange(event) {
+        const filterText = event.target.value;
+        this.setState({
+            filteredData: this.filterData(filterText, this.originalData)
+        });
+    }
+
+    filterData(filterText, data) {
+        if (filterText === null || filterText === undefined || filterText.trim() === '') {
+            return this.originalData;
+        }
+        const result = [];
+        data.forEach(products => {
+            const filteredProducts = [];
+            products.items.forEach(product => {
+                if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1) {
+                    filteredProducts.push(product);
+                }
+            });
+            if (filteredProducts.length > 0) {
+                result.push({
+                    category: products.category,
+                    items: filteredProducts,
+                })
+            }
+        });
+        return result;
     }
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onFilterTextChange={this.handleFilterTextChange}/>
                 <ProductTable data={this.state.filteredData}/>
             </div>
         );
