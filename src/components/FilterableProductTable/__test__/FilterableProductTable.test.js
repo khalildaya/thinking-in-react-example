306e0e0a4c794674e1f9e7f055b9d9f409837ad9
Khalil Daya <khalildaya@Khalils-MacBook-Pro.local>
2019-10-14 18:35:09 -0400

    adds paramterixed tests to cover 3 more cases

--- src/components/FilterableProductTable/__test__/FilterableProductTable.test.js
+++ src/components/FilterableProductTable/__test__/FilterableProductTable.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import FilterableProductTable from '../FilterableProductTable';
+import each from 'jest-each';
 import { exportAllDeclaration } from '@babel/types';
 
 function extractProductCategories(data) {
@@ -31,12 +32,12 @@ test('Component renders fine', () => {
         // Check that all component have rendered
         // Check text input in search bar and expect it to be null
         const filterTextInput = getByPlaceholderText("Search...");
-        expect(filterTextInput.nodeValue).toBeNull();
+        expect(filterTextInput.value).toEqual("");
 
-        // Check checkbox in search bar and expect it to be unchecked
+        // Check that checkbox exists in search bar and expect it to be unchecked
         const filterCheckboxInput = getByLabelText("Only show products in stock");
         expect(filterCheckboxInput.checked).toBeFalsy();
-        
+
         // Check for ProductTable component
         getByText("Name");
         getByText("Price");
@@ -52,4 +53,97 @@ test('Component renders fine', () => {
             getByText(product.name);
             getByText(product.price);
         })
+});
+
+test('Non-stocked items disapear when checkbox is checked', () => {
+    const products = [
+        {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+        {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+        {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+        {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+        {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+        {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+      ];
+
+    const {
+        getByLabelText,
+        getByText,
+    } = render(<FilterableProductTable data={products}/>);
+
+    /*
+        Make sure that checkbox is not checked after component renders
+        then send a click event to the checkox to filter out non-stocked items
+    */
+    const filterCheckboxInput = getByLabelText("Only show products in stock");
+    expect(filterCheckboxInput.checked).toBeFalsy();
+    fireEvent.click(filterCheckboxInput);
+    expect(filterCheckboxInput.checked).toBeTruthy();
+
+    // Expect to find all stocked items and no non-stocked items
+    products.forEach(product => {
+        if (product.stocked) {
+            getByText(product.name);
+            getByText(product.price);
+        } else {
+            console.log(product.name);
+            expect(() => {
+                getByText(product.name);
+            }).toThrow();
+        }
+    });
+});
+
+/*
+    A paramterized test to cover the following cases
+    1. No filter text
+    2. filter text that results in some non-stocked products whcih should not be displayed due to filter chekbox begin checked
+    3. Filter text that results in no products to display regardless of checkbox state 
+*/
+each(["","ball", "noitems"]).test('Product list does filter based on filter text and stocked only checkbox state', (filterText) => {
+    const products = [
+        {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+        {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+        {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+        {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+        {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+        {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+      ];
+
+    const {
+        getByLabelText,
+        getByText,
+        getByPlaceholderText,
+    } = render(<FilterableProductTable data={products}/>);
+
+    /*
+        Make sure that checkbox is not checked after component renders
+        then send a click event to the checkox to filter out non-stocked items
+    */
+   const filterCheckboxInput = getByLabelText("Only show products in stock");
+   expect(filterCheckboxInput.checked).toBeFalsy();
+   fireEvent.click(filterCheckboxInput);
+   expect(filterCheckboxInput.checked).toBeTruthy();
+    
+    const filterTextInput = getByPlaceholderText("Search...");
+    expect(filterTextInput.nodeValue).toBeNull();
+    fireEvent.change(filterTextInput, {
+        target: {
+            value: filterText,
+        },
+    });
+    expect(filterTextInput.value).toEqual(filterText);
+    // Expect to find all products with the string "ball" in their name except non-stocked ones
+    products.forEach(product => {
+        if(product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+            expect(() => {
+                getByText(product.name);
+            }).toThrow();
+        } else if(!product.stocked) {
+            expect(() => {
+                getByText(product.name);
+            }).toThrow();
+        } else {
+            getByText(product.name);
+        }
+    });
 });
